481fbadc1f2ec7c18a1c1ee08e07d4eb31225db9
Chinwe <renqingwei123@163.com>
2018-01-28 17:28:22 +0800

    try autowire from config class

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -1,9 +1,22 @@
 package soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages={"soundsystem"})
 public class CDPlayerConfig {
+
+    @Bean(name="lonelyHeartsClubBand")
+    public CompactDisc sgtPeppers(){
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CDPlayer cdPlayer(){
+        return new CDPlayer(sgtPeppers());
+    }
+
+
 }
