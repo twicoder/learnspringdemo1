943a6c84c4476428272330f3b31ab0017356e625
twicoder <renqingwei123@163.com>
2018-01-28 11:11:34 +0800

    Basic Spring Project

---
+++ src/test/java/com/springinaction/knights/BraveKnightTest.java
@@ -0,0 +1,19 @@
+package com.springinaction.knights;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class BraveKnightTest {
+
+
+    @Test
+    public void knightShouldEmbarkOnQuest(){
+        Quest mockQuest = mock(Quest.class);
+        BraveKnight knight = new BraveKnight(mockQuest);
+        knight.embarkOnQuest();
+        verify(mockQuest,times(1)).embark();
+    }
+}
